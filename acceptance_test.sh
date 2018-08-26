6d9b1859dd3b2af96e19b540b5c92721ab89b911
Ubuntu <ubuntu@ip-172-31-22-75.ap-south-1.compute.internal>
2018-08-26 13:34:55 +0000

    a

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-test $(curl http://ec2-13-232-158-67.ap-south-1.compute.amazonaws.com:8765/DevOpsCal/add?a=10\&b=20) -eq 30
+test $(curl http://ec2-52-66-166-134.ap-south-1.compute.amazonaws.com:8765/DevOpsCal/add?a=10\&b=20) -eq 30
