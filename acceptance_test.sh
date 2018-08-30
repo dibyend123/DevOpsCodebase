5b47037ff779247c20073f0a0c976faf0f3c78d6
Ubuntu <ubuntu@ip-172-31-22-75.ap-south-1.compute.internal>
2018-08-30 16:09:28 +0000

    a

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 CALCULATOR_PORT=$(docker-compose port calculator 8080 | cut -d: -f2)
-test $(curl ec2-13-232-124-44.ap-south-1.compute.amazonaws.com:$CALCULATOR_PORT/sum?a=1\&b=2) -eq 3
+test $(curl ec2-13-126-228-146.ap-south-1.compute.amazonaws.com:$CALCULATOR_PORT/sum?a=1\&b=2) -eq 3
