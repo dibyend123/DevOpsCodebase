bc08dbbd526c86038d13a813c4b105849f3d831a
Ubuntu <ubuntu@ip-172-31-22-75.ap-south-1.compute.internal>
2018-09-01 14:13:27 +0000

    a

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-CALCULATOR_PORT=$(docker-compose port calculator 4000 | cut -d: -f2)
-test $(curl ec2-52-66-15-56.ap-south-1.compute.amazonaws.com:$CALCULATOR_PORT/sum?a=1\&b=2) -eq 3
+CALCULATOR_PORT=$(docker-compose port cipipeline_calculator_1 4000 | cut -d: -f2)
+test $(curl ec2-52-66-15-56.ap-south-1.compute.amazonaws.com:4000/sum?a=1\&b=2) -eq 3
